b9a1417b8762dec6132d1aba83a3f0178911773a
hamzabell <akanbassey8@gmail.com>
2020-11-23 17:19:19 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        logo: ['Modak']
+      },
+      colors:  {
+        afrkblue: '#261D56',
+        afrkgreen: "#23B682",
+        afrkpurple: '#C2B7EE'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
