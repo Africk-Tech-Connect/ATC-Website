f7b6647f925ec1dbf7d79c4f1a88a2c0b213bc5b
hamzabell <akanbassey8@gmail.com>
2020-12-01 01:27:45 +0100

    some mo changes

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -1,10 +1,14 @@
 const burger = document.querySelector('#burger');
 const menu  = document.querySelector('#menu');
 
+console.log(menu)
+
 burger.addEventListener('click', () => {
     if(menu.classList.contains('hidden')){
         menu.classList.remove('hidden');
     } else {
         menu.classList.add('hidden');
     }
-})
+});
+
+console.log('heerrrer')
