6e0c99c3b1fd7d943af60cf9bb58513ce30ada35
hamzabell <akanbassey8@gmail.com>
2020-12-01 01:54:43 +0100

    cancel button

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -1,14 +1,22 @@
 const burger = document.querySelector('#burger');
 const menu  = document.querySelector('#menu');
+const cancel  = document.querySelector('#cancel');
 
-console.log(menu)
 
 burger.addEventListener('click', () => {
     if(menu.classList.contains('hidden')){
         menu.classList.remove('hidden');
-    } else {
-        menu.classList.add('hidden');
+        cancel.classList.remove('hidden')
+        burger.classList.add('hidden')
     }
 });
 
+
+cancel.addEventListener('click', () => {
+    if(!menu.classList.contains('hidden')){
+        menu.classList.add('hidden');
+        cancel.classList.add('hidden')
+        burger.classList.remove('hidden')
+    }
+});
 console.log('heerrrer')
