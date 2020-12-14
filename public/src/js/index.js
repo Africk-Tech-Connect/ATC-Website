1c7b509d121b3134c72fcccb4cd4aeadb850191e
hamzabell <akanbassey8@gmail.com>
2020-12-14 02:58:30 +0100

    changes

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -3,6 +3,7 @@ const menu  = document.querySelector('#menu');
 const cancel  = document.querySelector('#cancel');
 
 
+
 burger.addEventListener('click', () => {
     if(menu.classList.contains('hidden')){
         menu.classList.remove('hidden');
@@ -19,4 +20,34 @@ cancel.addEventListener('click', () => {
         burger.classList.remove('hidden')
     }
 });
-console.log('heerrrer')
+
+
+
+const  openLink = (link) =>{
+    window.open(link, '_blank');
+}
+
+const showSlides = (n) => {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+}
+const plusSlides =(n) => {
+    showSlides(slideIndex += n);
+  }
+  
+const currentSlide = (n) => {
+    showSlides(slideIndex = n);
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
